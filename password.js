20c64be003b8e196f9bc6bf5d15a7abdb7a266e8
Anson Lai <z5361987@ad.unsw.edu.au>
2022-06-13 21:41:35 +1000

    Update password.js & password.test.js

--- password.js
+++ password.js
@@ -21,6 +21,11 @@
  *     - passwords that are exactly any of the top 5 (not 20) passwords
  *     from the 2021 Nordpass Ranking:
 *      - https://en.wikipedia.org/wiki/List_of_the_most_common_passwords
+       123456
+       123456789
+       12345
+       qwerty
+       password
  * - "Poor Password"
  *     - any password that is not horrible, moderate or strong.
  *
@@ -28,10 +33,37 @@
  * @returns {string} string to indicate the strength of the password.
  */
 export function checkPassword(password) {
-  // FIXME:
-  return '';
+  // Horrible
+  if (password === '123456') return 'Horrible password';
+  if (password === '123456789') return 'Horrible password';
+  if (password === '12345') return 'Horrible password';
+  if (password === 'qwerty') return 'Horrible password';
+  if (password === 'password') return 'Horrible password';
+  // Strong 
+  breakme: if (password.length >= 12) {
+    if (!containsNumber(password)) break breakme;
+    if (!hasLowerCase(password)) break breakme;
+    if (!hasUpperCase(password)) break breakme;
+    return 'Strong password';
+  }
+  // Moderate 
+  breakme: if (password.length >= 8) {
+    if (!containsNumber(password)) break breakme;
+    return 'Moderate password';
+  }
+  // Poor
+  return 'Poor password';
 }
 
+function containsNumber(str) {
+  return /\d/.test(str);
+}
+function hasLowerCase(str) {
+  return (/[a-z]/.test(str));
+}
+function hasUpperCase(str) {
+  return (/[A-Z]/.test(str));
+}
 /**
  * Testing will no longer be done in here.
  * See password.test.js
--- password.test.js
+++ password.test.js
@@ -5,9 +5,53 @@
 
 import { checkPassword } from './password';
 
-test('Remove this test and uncomment the other tests below', () => {
-  expect(1 + 1).toEqual(2);
-});
+describe('Horrible password', () => {
+  test.each`
+    input     | expectedResult
+    ${'123456'}  | ${'Horrible password'}
+    ${'123456789'}   | ${'Horrible password'}
+    ${'12345'}  | ${'Horrible password'}
+    ${'qwerty'}   | ${'Horrible password'}
+    ${'password'} | ${'Horrible password'}
+    // add new test cases here
+  `('password $input is $expectedResult', ({ input, expectedResult }) => {
+    expect(checkPassword(input)).toBe(expectedResult)
+  })
+})
+
+describe('Length', () => {
+  test.each`
+    input     | expectedResult
+    ${'Comp1531AAAA'}  | ${'Strong password'}
+    ${'Comp1531'}   | ${'Moderate password'}
+    ${'Comp'}  | ${'Poor password'}
+    // add new test cases here
+  `('password $input is $expectedResult', ({ input, expectedResult }) => {
+    expect(checkPassword(input)).toBe(expectedResult)
+  })
+})
+describe('has number', () => {
+  test.each`
+    input     | expectedResul
+    ${'CompOneFiveThreeOne'}   | ${'Poor password'}
+    ${'CompOneFive31'} | ${'Strong password'}
+    ${'CompOneF3'} | ${'Moderate password'}
+    // add new test cases here
+  `('password $input is $expectedResult', ({ input, expectedResult }) => {
+    expect(checkPassword(input)).toBe(expectedResult)
+  })
+})
+describe('Uppercase and lowercase', () => {
+  test.each`
+    input     | expectedResul
+    ${'CompOneFIve3111'}   | ${'Strong password'}
+    ${'componefive3111'} | ${'Moderate password'}
+    ${'COMPFIVE3111'} | ${'Moderate password'}
+    // add new test cases here
+  `('password $input is $expectedResult', ({ input, expectedResult }) => {
+    expect(checkPassword(input)).toBe(expectedResult)
+  })
+})
 
 /*
 
